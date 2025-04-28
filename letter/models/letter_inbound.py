077771f0f302c3b5ee1428a14824f45171c817a4
quantum-pinto <apinto@quantumgroupgh.com>
2025-04-28 09:29:23 +0000

    letter: reply inbound letters

---
+++ letter/models/letter_inbound.py
@@ -0,0 +1,151 @@
+from random import randint
+
+from odoo import api, fields, models
+
+
+class LetterInbound(models.Model):
+    _name = "letter.inbound"
+    _description = "Letter Inbound"
+
+    _check_company_auto = True
+
+    def _get_default_color(self):
+        return randint(1, 11)
+
+    name = fields.Char(required=True, string="Reference")
+    date = fields.Date(default=fields.Date.today, help="When the letter is to be sent")
+    description = fields.Text()
+    attachment = fields.Binary(string="Letter")
+    user_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Sender",
+        # Default to the current user's partner
+        default=lambda self: self.env.user.partner_id,
+    )
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        string="Company",
+        default=lambda self: self.env.company,
+    )
+    partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Recipient",
+    )
+    color = fields.Integer(default=lambda self: self._get_default_color())
+    status = fields.Selection(
+        default="pending",
+        readonly=True,
+        selection=[
+            ("sent", "Sent"),
+            ("pending", "Pending"),
+            ("failed", "Failed"),
+        ],
+    )
+    active = fields.Boolean(default=True)
+    is_delivered = fields.Boolean(default=False)
+    attachment_id = fields.Many2one(
+        comodel_name="ir.attachment",
+        string="Attachment",
+        readonly=True,
+        compute="_compute_attachment_id",
+    )
+    is_sender = fields.Boolean(default=True)
+
+    @api.depends("attachment")
+    def _compute_attachment_id(self):
+        for record in self:
+            if record.attachment:
+                attachment = self.env["ir.attachment"].create(
+                    {
+                        "name": f"{record.name}.pdf",
+                        "type": "binary",
+                        "datas": record.attachment,
+                        "res_model": self._name,
+                        "res_id": record.id,
+                        "mimetype": "application/pdf",
+                    }
+                )
+                record.attachment_id = attachment.id
+
+    def read(self, fields=None, load="_classic_read"):
+        if fields and "company_id" not in fields:
+            fields.append("company_id")
+
+        records = super().read(fields=fields, load=load)
+        company_ids = self.env.companies.ids
+
+        records = [record for record in records if record["company_id"] in company_ids]
+        return records
+
+    def mail_recipient(self):
+        try:
+            mail_values = {
+                "subject": f"New Inbound Letter: {self.name}",
+                "body_html": f"""
+                    <p>Dear {self.partner_id.name},</p>
+                    <p>You have a new inbound letter.</p>
+                    <p>Reference: {self.name}</p>
+                    <p>Description: {self.description}</p>
+                """,
+                "email_to": self.partner_id.email,
+            }
+            mail = self.env["mail.mail"].create(mail_values)
+            mail.send()
+            # print("Email sent successfully")
+            return True
+        except Exception:
+            return False
+
+    def send_inbound_letter(self):
+        try:
+            self.env["letter.inbound"].create(
+                {
+                    "name": self.name,
+                    "date": fields.Date.today(),
+                    "description": self.description,
+                    "attachment": self.attachment,
+                    "user_id": self.partner_id.id,
+                    "company_id": self.company_id.id,
+                    "partner_id": self.user_id.id,
+                    "status": "sent",
+                    "is_sender": False,
+                    "is_delivered": True,
+                }
+            )
+            self.is_delivered = True
+            self.status = "sent"
+
+            self.mail_recipient()
+
+            return True
+        except Exception:
+            self.is_delivered = False
+            self.status = "failed"
+
+        return True
+
+    def send_inbound_letter_cron(self):
+        inbound_letters = self.search([])
+        for record in inbound_letters:
+            if not record.is_delivered and record.date <= fields.Date.today():  # type: ignore
+                record.send_inbound_letter()
+        return True
+
+    def action_send_inbound_letter(self):
+        self.ensure_one()
+        self.send_inbound_letter()
+        return True
+
+    def action_reply_inbound_letter(self):
+        self.ensure_one()
+        # return {
+        #     "type": "ir.actions.act_window",
+        #     "name": "Reply to Inbound Letter",
+        #     "res_model": "letter.inbound",
+        #     "view_mode": "form",
+        #     "view_type": "form",
+        #     "res_id": self.id,
+        #     "target": "current",
+        # }
+
+        return True
